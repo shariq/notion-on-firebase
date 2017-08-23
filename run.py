795640528074445a591e92e7de796c2e30575c5a
Shariq Hashme <shariqhashme@gmail.com>
2017-08-23 17:41:09 -0500

    warning for running without ga.js

--- run.py
+++ run.py
@@ -65,9 +65,19 @@ def main(root_page, firebase_path):
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print 'usage: python run.py <root_page> <firebase_path>'
+        print 'usage: python run.py <root_notion_page_id> <firebase_path>'
         print 'e.g, python run.py d065149ff38a4e7a9b908aeb262b0f4f ../firebase'
         sys.exit(-1)
     firebase_path = sys.argv[-1]
+    if not os.path.exists(firebase_path):
+        print 'error: that firebase_path could not be found. '
+        print '(path evaluated to {})'.format(os.path.abspath(firebase_path))
+        sys.exit(-1)
+    firebase_public_path = get_firebase_public_path(firebase_path)
+    if not os.path.exists(os.path.join(firebase_public_path, 'ga.js')):
+        print 'warning: ga.js was not found in your firebase public path'
+        print 'hit enter after placing it there or if you don\'t want ga.js'
+        print '(hint: this is a JS file from Google Analytics)'
+        raw_input()
     root_page = sys.argv[-2]
     main(root_page, firebase_path)
